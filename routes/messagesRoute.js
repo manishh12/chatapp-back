b1eead0080eb132353fe57dfde935483ca747b15
manishh12 <manishdid360@gmail.com>
2023-07-08 00:24:14 +0530

    first commit

---
+++ routes/messagesRoute.js
@@ -0,0 +1,8 @@
+const{ addMessage, getAllMessage } = require("../controllers/messagesController");
+
+const router =require("express").Router();
+router.post("/addmessage/",addMessage);
+router.post("/getmessage/",getAllMessage);
+
+
+module.exports=router;
